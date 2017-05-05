84339069b45bc9e2f08cc22b39eae59857cb65dc
Nicolas Badoux <badouxn@student.ethz.ch>
2017-05-05 18:06:15 +0200

    Startting

--- ringCT.sol
+++ ringCT.sol
@@ -1,33 +1,37 @@
 pragma solidity ^0.4.0;
 contract RingCT {
-    struct Voter {
-        uint weight;
-        bool voted;
-        uint8 vote;
-        address delegate;
-    }
-    struct Proposal {
-        uint voteCount;
-    }
 
-    address chairperson;
-    mapping(address => Voter) voters;
-    Proposal[] proposals;
 
-    /// Create a new ballot with $(_numProposals) different proposals.
-    function RingCT(uint8 _mixInNr, uint8 amount) {
-        chairperson = msg.sender;
-        voters[chairperson].weight = 1;
-        proposals.length = _numProposals;
+    Gx = 55066263022277343669578718895168534326250603453777594175500187360389116729240
+    Gy = 32670510020758816978083085130507043184471273380659243275938904335757337482424
+    P = -4294968273
+    ECADD = 5
+    ECMUL = 6
+    MODEXP = 7
+
+    kimage[] memory keyImagesUsed = new kimage[];
+
+    function verifyRing return boolean (bytes keyImage, string m, bytes32 keyMatrix) {
+
     }
 
 
-    function winningProposal() constant returns (uint8 winningProposal) {
-        uint256 winningVoteCount = 0;
-        for (uint8 proposal = 0; proposal < proposals.length; proposal++)
-            if (proposals[proposal].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[proposal].voteCount;
-                winningProposal = proposal;
+    function verifyRangeProofs return boolean () {
+    }
+
+    function verify  (bytes keyImage, string m, bytes32 keyMatrix, bytes32 rangeProofs) returns (boolean){
+        for(uint256 i = 0; i < keyImagesUsed.lenngth; i++) {
+            if(keyImagesUsed[i] == keyImage) {
+                return false;
             }
+        }
+        if(!verifyRangeProofs(rangeProofs)) {
+            return false;
+        } 
+        if(!verifyRing(keyImage, m, keyMatrix)) {
+            return false;
+        }
+        keyImagesUsed.push(keyImage);
     }
+
 }
