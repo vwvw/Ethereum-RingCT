0627d11c1c55306a2caf082d29b15383a43ab287
Nicolas Badoux <badouxn@student.ethz.ch>
2017-05-07 00:16:47 +0200

    Going to bed

--- ringCT.sol
+++ ringCT.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.4.0;
 contract RingCT {
 
+    event PrintString(
+        string _value
+    );
+
 
     Gx = 55066263022277343669578718895168534326250603453777594175500187360389116729240
     Gy = 32670510020758816978083085130507043184471273380659243275938904335757337482424
@@ -11,8 +15,17 @@ contract RingCT {
 
     kimage[] memory keyImagesUsed = new kimage[];
 
-    function verifyRing return boolean (bytes keyImage, string m, bytes32 keyMatrix) {
+    function verifyRing return boolean (bytes pubKeys, string m, bytes32 keyMatrix) {
+        numberInput = keyMatrix.length;
 
+        if(pubKeys.length != numberInput) {
+            PrintString("the number of public keys and the size of the ring dont match")
+            return false;
+        }
+
+        for(uint256 i = 1; i < numberInput; i++ {
+
+        }
     }
 
 
@@ -20,7 +33,7 @@ contract RingCT {
     }
 
     function verify  (bytes keyImage, string m, bytes32 keyMatrix, bytes32 rangeProofs) returns (boolean){
-        for(uint256 i = 0; i < keyImagesUsed.lenngth; i++) {
+        for(uint256 i = 0; i < keyImagesUsed.length; i++) {
             if(keyImagesUsed[i] == keyImage) {
                 return false;
             }
@@ -32,6 +45,10 @@ contract RingCT {
             return false;
         }
         keyImagesUsed.push(keyImage);
+
+
+        /* inscribe the transaction in the block chain
+         */
     }
 
 }
