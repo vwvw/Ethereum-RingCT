d129cf4076cd303e2267fc50c99e20833a0000a2
Nicolas Badoux <badouxn@student.ethz.ch>
2017-05-09 17:09:13 +0200

    Setup instructions and compilable draft

---
+++ Setup.md
@@ -0,0 +1,21 @@
+#Init blockchain for two nodes in two different directory
+#Run this command in both nodes
+geth --datadir . init ../eth/genesis.json
+
+
+#Run one of these command in two terminal window
+
+geth --identity "MyNodeName" --rpc --rpcport "8080" --rpccorsdomain "*" --datadir "~/ethereum/node1" --port "30303" --nodiscover --rpcapi "db,eth,net,web3" --networkid 1999 console
+
+
+geth --identity "MyNodeName" --rpc --rpcport "8081" --rpccorsdomain "*" --datadir "~/ethereum/node2" --port "30304" --nodiscover --rpcapi "db,eth,net,web3" --networkid 1999 console
+
+#Peers should be empty
+admin.peers
+
+#Get enode of one node
+admin.nodeInfo.enode
+
+#Connect both nodes by entering in one terminal, with the enode outputted by the previous function
+admin.addPeer("output of nodeInfo.enode")
+
--- ringCT.sol
+++ ringCT.sol
@@ -1,38 +1,38 @@
 pragma solidity ^0.4.0;
 contract RingCT {
 
-    event PrintString(
-        string _value
-    );
+    event PrintString(address indexed _from, string _value);
 
 
-    Gx = 55066263022277343669578718895168534326250603453777594175500187360389116729240
-    Gy = 32670510020758816978083085130507043184471273380659243275938904335757337482424
-    P = -4294968273
-    ECADD = 5
-    ECMUL = 6
-    MODEXP = 7
+    int256 Gx = 55066263022277343669578718895168534326250603453777594175500187360389116729240;
+    int256 Gy = 32670510020758816978083085130507043184471273380659243275938904335757337482424;
+    int256 P = -4294968273;
+    int256 ECADD = 5;
+    int256 ECMUL = 6;
+    int256 MODEXP = 7;
+    
+    bytes32[] keyImagesUsed;
 
-    kimage[] memory keyImagesUsed = new kimage[];
 
-    function verifyRing return boolean (bytes pubKeys, string m, bytes32 keyMatrix) {
-        numberInput = keyMatrix.length;
+
+    function verifyRing (bytes pubKeys, string m, bytes32 keyMatrix) returns (bool) {
+        uint256 numberInput = keyMatrix.length;
 
         if(pubKeys.length != numberInput) {
-            PrintString("the number of public keys and the size of the ring dont match")
+
             return false;
         }
 
-        for(uint256 i = 1; i < numberInput; i++ {
+        for(uint256 i = 1; i < numberInput; i++) {
 
         }
     }
 
 
-    function verifyRangeProofs return boolean () {
+    function verifyRangeProofs (bytes32 rangeProofs) returns (bool) {
     }
 
-    function verify  (bytes keyImage, string m, bytes32 keyMatrix, bytes32 rangeProofs) returns (boolean){
+    function verify  (bytes32 keyImage, bytes pubKeys, string m, bytes32 keyMatrix, bytes32 rangeProofs) returns (bool){
         for(uint256 i = 0; i < keyImagesUsed.length; i++) {
             if(keyImagesUsed[i] == keyImage) {
                 return false;
@@ -41,7 +41,7 @@ contract RingCT {
         if(!verifyRangeProofs(rangeProofs)) {
             return false;
         } 
-        if(!verifyRing(keyImage, m, keyMatrix)) {
+        if(!verifyRing(pubKeys, m, keyMatrix)) {
             return false;
         }
         keyImagesUsed.push(keyImage);
